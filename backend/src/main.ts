5ee1eb154ca2f1adb16b2e38af2e188da2cdb705
Bassem MSAYIF <bassem.msayif@etu.univ-amu.fr>
2022-03-05 17:56:46 +0100

    zebi lol sdfgqdgd

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import express, { Express } from "express";
 const app: Express = express();
 const port: number = parseInt(process.env.PORT || "3200");
 
+const elasticsearch = require("elasticsearch");
+const client = new elasticsearch.Client({
+  host: "http://localhost:9200",
+  log: "trace",
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port} 🚀`);
 });

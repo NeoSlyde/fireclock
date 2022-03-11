78504c97dbd8bfd0cd7bbb170f1f693e141c2416
Bassem MSAYIF <bassem.msayif@etu.univ-amu.fr>
2022-03-11 21:52:22 +0100

    lol
    Co-authored-by: LHashDe <LHashDe@users.noreply.github.com>

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthService } from "./auth/auth.service";
@@ -8,10 +9,18 @@ import { DummyAuthService } from "./auth/dummy_auth.service";
 import { LangEnService } from "./lang/lang-en.service";
 import { LangService } from "./lang/lang.service";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [
     { provide: LangService, useClass: LangEnService },
     { provide: AuthService, useClass: DummyAuthService },

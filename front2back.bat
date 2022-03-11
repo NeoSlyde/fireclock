7055411dc8a9aaa3793ad3ead4aa68e8fe5d3b18
Diego Imbert <diegoimbert@pm.me>
2022-03-11 22:29:47 +0100

    Fix conflict

--- front2back.bat
+++
@@ -1,4 +0,0 @@
-cd frontend
-del /f /s /q dist
-npm run build
-xcopy /s /i /e dist ..\backend\dist
--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
 import { LangService } from "../lang/lang.service";
-import { HttpService } from "../service/http.service";
 
 @Component({
   selector: "app-navbar",
@@ -10,32 +7,6 @@ import { HttpService } from "../service/http.service";
   styleUrls: ["./navbar.component.sass"],
 })
 export class NavbarComponent implements OnInit {
-  userLogin: FormGroup;
-
-  constructor(
-    readonly langService: LangService,
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private httpService: HttpService
-  ) {
-    this.userLogin = formBuilder.group({
-      title: formBuilder.control("initial value", Validators.required),
-    });
-  }
-
-  ngOnInit(): void {
-    this.initForm();
-  }
-
-  initForm() {
-    this.userLogin = this.formBuilder.group({
-      userName: ["", Validators.required],
-      password: ["", Validators.required],
-    });
-  }
-
-  onSubmitForm() {
-    const formValue = this.userLogin.value;
-    console.log(formValue);
-  }
+  constructor(readonly langService: LangService) {}
+  ngOnInit(): void {}
 }

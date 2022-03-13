324aa7d2d495500e1b4159145ad39f3399d42b97
Diego Imbert <diegoimbert@pm.me>
2022-03-13 23:04:53 +0100

    Update behavior subject in HttpActivityService

--- frontend/src/app/activities/http-activity.service.ts
+++ frontend/src/app/activities/http-activity.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, firstValueFrom, Observable } from "rxjs";
-
 import { Activity, ActivityService } from "./activity.service";
 
 const jsonContentTypeOptions = {
@@ -15,6 +14,16 @@ const jsonContentTypeOptions = {
 })
 export class HttpActivityService extends ActivityService {
   _activitiesDb = new BehaviorSubject<Activity[]>([]);
+
+  _updateById(id: string, update: (t: Activity) => Activity | null) {
+    this._activitiesDb.next(
+      this._activitiesDb.value.flatMap((a) => {
+        if (a.id !== id) return [a];
+        const x = update(a);
+        return x !== null ? [x] : [];
+      })
+    );
+  }
 
   constructor(readonly http: HttpClient) {
     super();
@@ -43,6 +52,7 @@ export class HttpActivityService extends ActivityService {
           jsonContentTypeOptions
         )
       );
+      this._activitiesDb.next([...this._activitiesDb.value, activity]);
       return activity;
     } catch (error) {
       throw new Error("Error: Create Activity");
@@ -51,14 +61,15 @@ export class HttpActivityService extends ActivityService {
   override async delete(activityId: string): Promise<void> {
     try {
       const activity = await firstValueFrom(
-        this.http.post<Task>(
+        this.http.post(
           "/api/delete-activity",
           { activityId },
           jsonContentTypeOptions
         )
       );
+      this._updateById(activityId, (_) => null);
     } catch (error) {
-      throw new Error("Error: Delete Task");
+      throw new Error("Error: Delete Activity");
     }
   }
   override async updateDuration(
@@ -66,15 +77,16 @@ export class HttpActivityService extends ActivityService {
     duration: number
   ): Promise<void> {
     try {
-      const task = await firstValueFrom(
-        this.http.post<Task>(
+      const Activity = await firstValueFrom(
+        this.http.post(
           "/api/update-duration-activity",
           { activityId, duration },
           jsonContentTypeOptions
         )
       );
+      this._updateById(activityId, (a) => ({ ...a, duration }));
     } catch (error) {
-      throw new Error("Error: Create Task");
+      throw new Error("Error: Create Activity");
     }
   }
   override async updateCreated(
@@ -82,15 +94,16 @@ export class HttpActivityService extends ActivityService {
     created: Date
   ): Promise<void> {
     try {
-      const task = await firstValueFrom(
-        this.http.post<Task>(
+      const activity = await firstValueFrom(
+        this.http.post<Activity>(
           "/api/update-created-activity",
           { activityId, created },
           jsonContentTypeOptions
         )
       );
+      this._updateById(activityId, (a) => ({ ...a, created }));
     } catch (error) {
-      throw new Error("Error: Create Task");
+      throw new Error("Error: Create Activity");
     }
   }
 }
